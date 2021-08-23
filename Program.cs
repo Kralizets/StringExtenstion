76b67d8dd6fa30849c35265bd4193310510eb39c
Kralizets <7Wolf752@gmail.com>
2021-08-23 19:41:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringExtenstion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string notPalindrom = "abnmba";
+            string isPalindorm = "abnba";
+
+            Console.WriteLine("notPalindrom: " + notPalindrom.IsPalindrom());
+            Console.WriteLine("isPalindorm: " + isPalindorm.IsPalindrom());
+
+            Console.Read();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static bool IsPalindrom(this string h)
+        {
+            if (string.IsNullOrWhiteSpace(h) || h.Length < 1)
+                return false;
+
+            for (int i = 0, j = (h.Length - 1); i < h.Length / 2; i++, j--)
+            {
+                if (h[i] != h[j])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
